3b792e0b6a2c8f5af7af54e5caff3e510cdf0220
Jhonatan <johnn_lds@hotmail.com>
2017-07-23 15:16:40 -0300

    remove elasticsearch callback from loja

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -26,7 +26,7 @@ RailsAdmin.config do |config|
   # config.authenticate_with do
   #   warden.authenticate! scope: :user
   # end
-  
+
   config.current_user_method(&:current_user)
 
   config.actions do
